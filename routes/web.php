02782d13fe77064ae0441cef2777dd553ba771cd
Laura Romero <lauraromgar95@gmail.com>
2024-09-09 19:02:07 +0200

    Proyecto con plantilla sin personalizar, antes de crear el layout y componentes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MainController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+
+Route::get('/', [MainController::class, 'index'])->name('main.index');
